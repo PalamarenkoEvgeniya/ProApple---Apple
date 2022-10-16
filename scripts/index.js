03cc11b8e5f703eb9f0a6a48903b20a98e18f8c3
Eugenia Palamarenko <palamarenko.evgeniya@gmail.com>
2022-10-16 21:23:44 +0300

    add timer

--- scripts/index.js
+++ scripts/index.js
@@ -113,7 +113,7 @@ const showPrice = (currency = 'USD') => {
 };
 
 const myHeaders = new Headers();
-myHeaders.append('apikey', '.vWnI1llXinkQgvtUgLqjVgV0LrWWcd87');
+myHeaders.append('apikey', 'vWnI1llXinkQgvtUgLqjVgV0LrWWcd87');
 
 const requestOptions = {
   method: 'GET',
@@ -145,3 +145,74 @@ countryWrapper.addEventListener('click', ({ target }) => {
   }
   showPrice(target.dataset.currency);
 });
+
+//  timer
+
+const declOfNum = (n, titles) =>
+  titles[
+    n % 10 === 1 && n % 100 !== 11
+      ? 0
+      : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)
+      ? 1
+      : 2
+  ];
+
+const timer = (deadline) => {
+  const unitDay = document.querySelector('.timer__unit-day');
+  const unitHour = document.querySelector('.timer__unit-hour');
+  const unitMin = document.querySelector('.timer__unit-minute');
+  const descriptionDay = document.querySelector(
+    '.timer__unit--description-day',
+  );
+  const descriptionHour = document.querySelector(
+    '.timer__unit--description-hour',
+  );
+  const descriptionMin = document.querySelector(
+    '.timer__unit--description-minute',
+  );
+
+  const getTimeRemaning = () => {
+    const dateStop = new Date(deadline).getTime();
+    const dateNow = Date.now();
+    const timeRemaning = dateStop - dateNow;
+
+    const minutes = Math.floor((timeRemaning / 1000 / 60) % 60);
+    const hours = Math.floor((timeRemaning / 1000 / 60 / 60) % 24);
+    const days = Math.floor(timeRemaning / 1000 / 60 / 60 / 24);
+
+    return { timeRemaning, minutes, hours, days };
+  };
+
+  const start = () => {
+    const timer = getTimeRemaning();
+
+    unitDay.textContent = timer.days;
+    unitHour.textContent = timer.hours;
+    unitMin.textContent = timer.minutes;
+    descriptionDay.textContent = declOfNum(timer.days, ['день', 'дня', 'дней']);
+    descriptionHour.textContent = declOfNum(timer.hours, [
+      'час',
+      'часа',
+      'часов',
+    ]);
+    descriptionMin.textContent = declOfNum(timer.minutes, [
+      'минута',
+      'минуты',
+      'минут',
+    ]);
+
+    const timerId = setTimeout(start, 60000);
+
+    if (timer.timeRemaning < 0) {
+      clearTimeout(timerId);
+
+      unitDay.textContent = '0';
+      unitHour.textContent = '0';
+      unitMin.textContent = '0';
+    }
+  };
+
+  start();
+};
+
+timer('2023/09/07 20:00');
